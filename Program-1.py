6bedf3bebcf7aa3857c6792067ade1eea95b1e49
zaidlaffta <bscmsc70@yahoo.com>
2019-06-29 02:53:05 -0700

    Add files via upload

--- Program-1.py
+++ Program-1.py
@@ -1,4 +1,10 @@
-#This program used to remove redundant words in a sentence.
-s = input("Please input you text:   ")
-w = s.split(" ")
-print (" ".join(sorted(list(set(w)))))
+#this program to find the min, max, length and sort a list
+import random
+listx = []
+for x in range (34):
+    listx.append(x)
+print (listx)
+print ("The minimum number in the list is: ", min(listx))
+print ("The maximum number in the list is: ", max(listx))
+print ("the list length is: ", len(listx))
+print ("The sorted list is: ", sorted(listx))
